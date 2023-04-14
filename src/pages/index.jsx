5fffb1216bd5dd468ac57714dbcbe757f39d2ad7
borunx <jonatanfelizf@gmail.com>
2023-04-14 17:01:36 +0200

    card done

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,17 +2,19 @@ import Head from "next/head";
 import Image from "next/image";
 import { MediaContext } from "@/contexts/MediaContext";
 import { useContext } from "react";
+import MediaCard from './mediaCard';
 
 export default function Home() {
-  let data = useContext(MediaContext);
+  //let data = useContext(MediaContext);
   // let result;
 
   // getMedia_promise.then((promise_result) => {
   //   result = promise_result;
   // });
 
-  console.log(data[0].id);
+  // console.log(data[0].id);
   // let media1 = data[0];
+  
   return (
     <>
       <Head>
@@ -23,10 +25,12 @@ export default function Home() {
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
       </Head>
       <main>
         <h1>Context</h1>
-        <p>{data[0].id}</p>
+        {/* <p>{data[0].id}</p> */}
+        <MediaCard/>
       </main>
     </>
   );
