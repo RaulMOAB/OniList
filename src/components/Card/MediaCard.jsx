9a9bf351e8878308337b0b1b76f53681a61e82cb
RaulMOAB <raulmontoroabaijon@gmail.com>
2023-04-21 11:16:57 +0200

    responsive cards

--- src/components/Card/MediaCard.jsx
+++ src/components/Card/MediaCard.jsx
@@ -1,42 +1,44 @@
 /* eslint-disable @next/next/no-img-element */
-export default function MediaCard({ media ,index}) {
+export default function MediaCard({ media, index }) {
   //get parameters
   let genres = JSON.parse(media.genres).splice(0, 3).join(" ");
 
   let score = 7.9;
 
-  console.log(index)
+  console.log(index);
 
   return (
-		<>
-			<div
-				className={
-					index < 4
-						? "relative rounded-xl bg-cover w-fit"
-						: "relative rounded-xl bg-cover w-fit md:hidden lg:block"
-				}>
-				<img
-					src={media.large_banner_image}
-					className='rounded-xl'
-					alt='media image'
-				/>
-				<div className='absolute inset-x-0 bottom-0 h-16 rounded-b-lg text-white backdrop-blur-md'>
-					<div className='px-2'>
-						<div className='flex '>
-							<p className='text-xl-base mt-2 sm-2 w-4/6 truncate font-semibold'>
-								{media.title}
-							</p>
-							<div className='w-2/6 mt-2 text-right'>
-								<i
-									className=' fa-solid fa-star'
-									style={{ color: "#f5c211" }}></i>
-								<p className='inline-block'>{score}</p>
-							</div>
-						</div>
-						<p className='text-xs mt-1 ms-2 truncate'>{genres}</p>
-					</div>
-				</div>
-			</div>
-		</>
-	);
+    <>
+      <div
+        className={
+          index < 4
+            ? "relative rounded-md bg-cover w-fit"
+            : "relative rounded-md bg-cover w-fit md:hidden lg:block"
+        }
+      >
+        <img
+          src={media.large_banner_image}
+          className="rounded-md"
+          alt="media image"
+        />
+        <div className="absolute inset-x-0 bottom-0 h-16 rounded-b-md text-white backdrop-blur-md">
+          <div className="px-2">
+            <div className="flex ">
+              <p className="text-xl-base mt-2 sm-2 w-4/6 truncate font-semibold">
+                {media.title}
+              </p>
+              <div className="w-2/6 mt-2 text-right">
+                <i
+                  className=" fa-solid fa-star"
+                  style={{ color: "#f5c211" }}
+                ></i>
+                <p className="inline-block">{score}</p>
+              </div>
+            </div>
+            <p className="text-xs mt-1 ms-2 truncate">{genres}</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,7 +4,7 @@ import { useContext } from "react";
 import MediaCard from "../components/Card/MediaCard";
 import Hero from "../components/Hero/Hero";
 import LoadingCloud from "@/components/Loading/LoadingCloud";
-import Main from "@/components/Common/Main/Main";
+import Container from "@/components/Common/PageContainer/Container";
 
 export default function Home() {
 	let data = useContext(MediaContext);
@@ -33,8 +33,8 @@ export default function Home() {
 			<header>
 				<Hero media={media_data} />
 			</header>
-
-			<Main>
+    <Container>
+			<main className="pb-10 2xl:px-28  xl:px-16  lg:px-2 sm:px-4 px-4">
 				<div className='p-3 rounded-md bg-slate-700 my-5'>
 					<p className='text-center text-white'>Filters</p>
 				</div>
@@ -44,7 +44,7 @@ export default function Home() {
 					</h1>
 				</div>
 				{
-					<div className='grid grid-cols-2 lg:grid-cols-6 md:grid-cols-4 sm:grid-cols-3 gap-2 lg:gap-4 md:gap-4 '>
+					<div className='grid grid-cols-2 xl:grid-cols-6 lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-3 gap-4 sm:gap-4 lg:gap-4 md:gap-8 2xl:gap-10 xl:gap-6'>
 						{media_data.map((media, i) => {
 							return (
 								<MediaCard
@@ -56,7 +56,8 @@ export default function Home() {
 						})}
 					</div>
 				}
-			</Main>
+			</main>
+    </Container>
 		</>
 	);
 }
