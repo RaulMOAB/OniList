fc06c5f8cf03f0c5895f748db09776ba2f3db71a
AMGG13 <alvinmillergarciagarcia@gmail.com>
2023-05-19 00:37:13 +0200

    s

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,4 +1,4 @@
-import "@/styles/globals.css";
+import "../styles/globals.css";
 import Layout from "../layouts/Layout";
 import { MediaContextProvider } from "../contexts/MediaContext";
 import {AuthContextProvider} from '@/contexts/AuthContext'
